fd439ad59fcb6243e45119b0cd5e8197b8c000bc
Kehinde Adedokun <75108469+hi-heavens@users.noreply.github.com>
2023-12-21 21:36:07 +0100

    implemented authentication for creating users and protecting our users route

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,4 +1,6 @@
 const User = require("../models/user.model");
+const jwt = require("jsonwebtoken");
+const { promisify } = require("util");
 
 const createUser = async (req, res) => {
   const { full_name, email, phone_number, username, password } = req.body;
@@ -13,13 +15,29 @@ const createUser = async (req, res) => {
 
   const savedUser = await user.save();
 
+  console.log(savedUser._id);
+
+  const token = jwt.sign({ id: savedUser._id }, process.env.SECRET_SAUCE, {
+    expiresIn: process.env.EXPIRES,
+  });
+
   res.status(201).json({
     status: true,
-    message: savedUser,
+    token,
+    message: "User created successfully!!! 😎",
   });
 };
 
 const getUser = async (req, res) => {
+  console.log(req.decode);
+  const existUser = await User.findOne({ _id: req.decode.id });
+  if (!existUser) {
+    res.status(401).json({
+      status: false,
+      message: "You must register or login to see this user details",
+    });
+    return;
+  }
   const foundUser = await User.findOne({ _id: req.params.id });
   if (!foundUser) {
     res
@@ -40,4 +58,22 @@ const userUpdate = async (req, res) => {
   });
 };
 
-module.exports = { createUser, getUser, userUpdate };
+const protectUser = async function (req, res, next) {
+  const authorization = req.headers.authorization;
+  let token;
+  if (authorization && authorization.startsWith("Bearer")) {
+    token = authorization.split(" ")[1];
+  }
+  if (!token) {
+    res.status(401).json({
+      status: false,
+      message: "Bruh, why are you peeping? Login or register to continue!",
+    });
+    return;
+  }
+  const decode = await promisify(jwt.verify)(token, process.env.SECRET_SAUCE);
+  req.decode = decode;
+  next();
+};
+
+module.exports = { createUser, getUser, userUpdate, protectUser };
